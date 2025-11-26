51b18ad63708f2b0492be40228accf16c8256725
saogamer622-arch <saogamer622@gmail.com>
2025-11-26 09:08:17 +0700

    test2-ifelse

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    public class Program
+{
+
+    public static void Main()
+    {
+        int age = 25;
+        if (age < 13)
+        {
+            Console.WriteLine("ss");
+        }
+        else if(age <  20)
+        {
+            Console.WriteLine("sss");
+        }
+        else if(age <  60)
+        {
+            Console.WriteLine("sss+");
+        }
+        else
+        {
+            Console.WriteLine("up");
+        }
+    }
+}
